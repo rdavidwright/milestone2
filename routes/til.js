b06be18a437c7d8226dffc356962a3cb225559fe
Ralph <david.wright@aggiemail.usu.edu>
2016-03-05 18:16:38 -0700

    everything should be working

--- routes/til.js
+++ routes/til.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var entries = [
-  {slug:"How to pass class.", body: "Come to class, do your homework."},
-  {slug:"How to fail class.", body: "Don't do anything."},
-  {slug:"What to do in class.", body: "Listen well and take notes."}
+  {slug:"Cloning", body: "Today I learned how to clone a repository so I can access what is on my GITHUB. Let's try to make this work this time.", created_at: "some date"},
+  {slug:"Navigating", body: "Today I learned how to make the link back to the index page work. I just had to use the .. to bring it back up a level.", created_at: "some date"},
+  {slug:"Deploying", body: "Today I learned how to deploy using heroku. It took me quite a while. I finally had to start from scratch and it finally worked.", created_at: "2/13/2016"}
 ];
 
 /* 1 GET til listing. */
@@ -24,19 +24,25 @@ router.post('/', function(req, res, next) {
 });
 
 /* 4 entries/*update entry. */
-router.get('/update/:id', function(req, res, next) {
+router.get('/:id/update', function(req, res, next) {
   res.render('til/update', {title: "Update entry", id: req.params.id, entry: entries[req.params.id]});
 });
 
 /* 5 post to entries */
-router.post('/update/:id', function(req, res, next) {
+router.post('/:id', function(req, res, next) {
   entries[req.params.id] = req.body;
   res.render('til/index', {title: 'Update an entry', entries: entries});
 });
 
+/* Delete entry /entries/*number* /delete */
+router.get('/:id/delete', function(req, res, next){
+  var id = req.params.id
+  entries = entries.slice(0, id).concat(entries.slice(id+1, entries.length));
+  res.render('til/index', {title: 'Entries', entries: entries});
+});
+
 /* 6 entries/*some number*. */
 router.get('/:id', function(req, res, next) {
-  console.log(req.params.id);
   res.render('til/entry', {title: "an entry", entry: entries[req.params.id]});
 });
 
