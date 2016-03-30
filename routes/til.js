f3b476631aabb85ff03675eae3969fb91af5d224
rdavidwright <david.wright@aggiemail.usu.edu>
2016-03-29 20:06:06 -0600

    login and update

--- routes/til.js
+++ routes/til.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-var entries = [
-//  {slug:"Cloning", body: "Today I learned how to clone a repository so I can access what is on my GITHUB. Let's try to make this work this time.", created_at: "some date"},
-//  {slug:"Navigating", body: "Today I learned how to make the link back to the index page work. I just had to use the .. to bring it back up a level.", created_at: "some date"},
-//  {slug:"Deploying", body: "Today I learned how to deploy using heroku. It took me quite a while. I finally had to start from scratch and it finally worked.", created_at: "2/13/2016"}
-];
+var entries = [];
 
 /* 1 GET til listing. */
 router.get('/', function(req, res, next) {
+  console.log(req.cookies.username);
+  var name = req.cookeis.username || 'anonmous';
   req.db.driver.execQuery(
     "SELECT * FROM til;"
     function(err, data){
@@ -18,7 +16,6 @@ router.get('/', function(req, res, next) {
       res.render('til/index', { title: 'Entries', entries: data});
     }
   );
-  // res.render('til/index', { title: 'Entries', entries: entries});
 });
 
 /* 2 til/new */
@@ -29,22 +26,13 @@ router.get('/new', function(req, res, next) {
 /* 3 til/ entry. */
 router.post('/', function(req, res, next) {
   req.db.driver.execQuery(
-    "INSERT INTO til (slug, body) VALUES ('" + req.body.slug + "','" + req.body.body + "');",
-    function(err, data){
-      if(err)
-      {
-        console.log(err);
-      }
-//      res.render('til/index', {title: 'Entries', entries: data});
-    }
-  );
-  req.db.driver.execQuery(
-    "SELECT * FROM til;",
+    "INSERT INTO til (slug, body) VALUES (?,?);",
+    [req.body.slug, req.body.body],
     function(err, data){
       if(err){
         console.log(err);
       }
-      res.render('til/index', {title: 'Entries', entries: data});
+      res.redirect(303, '/til/');
     }
   );
 });
@@ -52,7 +40,7 @@ router.post('/', function(req, res, next) {
 /* 4 entries/*update entry. */
 router.get('/:id/update', function(req, res, next) {
   req.db.driver.execQuery(
-    'SELECT * FROM til WHERE id=' + parseInt(req.params.id) + ';',
+    'SELECT * FROM til WHERE id=?;',
     function(err, data){
       if(err){
         console.log(err);
@@ -64,24 +52,15 @@ router.get('/:id/update', function(req, res, next) {
 
 /* 5 post to entries */
 router.post('/:id', function(req, res, next) {
-  var sqlstring = "UPDATE til SET slug='" + req.body.slug + "',body='" + req.body.body + "'WHERE id=" + parseInt(req.params.id) + ";";
-  console.log(sqlstring);
+  var id = parseInt(req.params.id);
   req.db.driver.execQuery(
-    sqlstring,
+    "UPDATE til SET slug = ?, body = ?, WHERE id = ?;",
+    [req.body.slug, req.body.body, parseInt(req.params.id)],
     function(err, data){
       if(err){
         console.log(err);
       }
-    }
-  );
-
-  req.db.driver.execQuery(
-    'SELECT * FROM til WHERE id=' + parseInt(req.params.id) + ';',
-    function(err, data){
-      if(err){
-        console.log(err);
-      }
-      res.render('til/entry', {title: "An Entry", entry: data[0]});
+      res.redirect(303, '/til/' + id);
     }
   );
 });
@@ -89,21 +68,13 @@ router.post('/:id', function(req, res, next) {
 /* Delete entry /entries/*number* /delete */
 router.get('/:id/delete', function(req, res, next){
   req.db.driver.execQuery(
-    'DELETE FROM til WHERE id=' + parseInt(req.params.id) + ';',
+    'DELETE FROM til WHERE id=?;',
+    [parseInt(req.params.id)],
     function(err, data){
       if(err){
         console.log(err);
       }
-//      res.render('til/index', {title: 'Entries', entries: data});
-    }
-  );
-  req.db.driver.execQuery(
-    "SELECT * FROM til;",
-    function(err, data){
-      if(err){
-        console.log(err);
-      }
-      res.render('til/index', {title: 'TIL', entries: data});
+      res.redirect(303, '/til/');
     }
   );
 });
@@ -111,7 +82,8 @@ router.get('/:id/delete', function(req, res, next){
 /* 6 entries/*some number*. */
 router.get('/:id', function(req, res, next) {
   req.db.driver.execQuery(
-    'SELECT * FROM til WHERE id=' + parseInt(req.params.id) + ';',
+    'SELECT * FROM til WHERE id=?;',
+    [parseInt(req.params.id)],
     function(err, data){
       if(err){
         console.log(err);
